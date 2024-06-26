74de514f1706205adddfd83d19a29e512a6cc3eb
OGEmanuel <1blacksanta416@gmail.com>
2024-06-26 11:11:55 +0100

    Opengraph attempt

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,28 @@ import Navbar from "./navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// HEAD - metadata setting
+export const metadata = {
+  metadataBase: new URL("https://trib-hub-website.vercel.app/"),
+  title: "Tribhub",
+  description: "Manage and Grow your communities seamlessly",
+  keywords:
+    "community, web development, online marketing, websites, e-commerce, marketing campaigns, SEO, SEM, social media marketing, lead generation, online growth",
+  robots: "index, follow",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+    other: {
+      rel: "apple-touch-icon-precomposed",
+      url: "/favicon.ico",
+    },
+  },
+  openGraph: {
+    images: "/og-image.png",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
